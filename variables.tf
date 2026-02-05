3f347b951d4f407ed986924eed42b21e82970a22
Ganga <ganga.m@suemdhait.com>
2026-02-05 17:05:42 +0530

    ami update

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "instance_name" {
 variable "ami_id" {
   description = "ami image"
   type        = string
-  default     = "ami-06004b171f1adb1a9"
+  default     = "ami-0c9c14fff526c5bda"
 }
 
 variable "name" {
