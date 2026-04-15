4e9dc8b2da8b9fc578da2c065bfd3548d37a62be
Ganga <ganga.m@suemdhait.com>
2026-04-15 12:57:35 +0530

    latest ami

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_name" {
 variable "ami_id" {
   description = "Lab AMI (bake semiconlabs-dcv-watch via golden-ami-dcv-watcher.sh before registering this id)"
   type        = string
-  default     = "ami-0a613a68d04dcdca1"
+  default     = "ami-0c38420cdc7470ef6"
 }
 
 variable "name" {
